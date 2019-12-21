456a26e0bb0c4cce627ce5ac83d69987b78eaee8
Rarysson Guilherme <r_guilherme12@hotmail.com>
2019-12-21 14:28:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pomodoro
+
+This project was made with HTML, CSS and JS.
+
+The [Pomodoro Technique](https://en.wikipedia.org/wiki/Pomodoro_Technique) consist in six steps:
+1. Decide on the task to be done.
+2. Set the pomodoro timer (traditionally to 25 minutes).
+3. Work on the task.
+4. End work when the timer rings and put a checkmark on a piece of paper.
+5. If you have fewer than four checkmarks, take a short break (3–5 minutes), then go to step 2.
+6. After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
+
+> From The Odin Project's [curriculum](https://www.theodinproject.com/lessons/pairing-project)
---
+++ script.js
@@ -0,0 +1,227 @@
+const default_break_time = 5;
+const default_work_time = 25;
+const default_pomodoro_qnt = 4;
+const text_work = "It's time to work!";
+const text_break = "It's break time!";
+
+const pomodoro = document.getElementById("quantity-pomodoro");
+const pomodoros_number = document.getElementById("pomodoros");
+const work_time = document.getElementById("work-time");
+const break_time = document.getElementById("break-time");
+const text_timer = document.getElementById("text-timer");
+const minutes_timer = document.getElementById("minutes");
+const seconds_timer = document.getElementById("seconds");
+const textarea = document.querySelector("textarea");
+
+let pomodoro_qnt = default_pomodoro_qnt;
+let work_qnt = default_work_time;
+let break_qnt = default_break_time;
+let minutes_work = work_qnt;
+let seconds_work = 0;
+let minutes_break = break_qnt;
+let seconds_break = 0;
+let pomodoros_count = pomodoro_qnt;
+let on_work = false;
+let on_break = false;
+let on_play = false;
+let paused = false;
+let id_interval;
+
+
+
+function correct_time(time) {
+	return (time < 10) ? `0${time}` : time;
+}
+
+function timer() {
+	if (paused) {
+		clearInterval(id_interval);
+	} else {
+		if (on_work) {
+			seconds_work--;
+	
+			if (seconds_work < 0) {
+				seconds_work = 59;
+				minutes_work--;
+			}
+	
+			if (minutes_work >= 0) {
+				minutes_timer.innerText = correct_time(minutes_work);
+				seconds_timer.innerText = correct_time(seconds_work);
+			} else {
+				minutes_work = work_qnt;
+				seconds_work = 0;
+				on_work = false;
+				on_break = true;
+
+				text_timer.innerText = text_break;
+				minutes_timer.innerText = correct_time(minutes_break);
+				seconds_timer.innerText = correct_time(seconds_break);
+				textarea.style.color = "darkslateblue";
+
+				let sound = new Audio("assets/break.mp3");
+				sound.play();
+			}
+		} else {
+			seconds_break--;
+	
+			if (seconds_break < 0) {
+				seconds_break = 59;
+				minutes_break--;
+			}
+	
+			if (minutes_break >= 0) {
+				minutes_timer.innerText = correct_time(minutes_break);
+				seconds_timer.innerText = correct_time(seconds_break);
+			} else {
+				pomodoros_count--;
+
+				minutes_break = break_qnt;
+				seconds_break = 0;
+				on_break = false;
+				on_work = true;
+
+				text_timer.innerText = text_work;
+				minutes_timer.innerText = correct_time(minutes_work);
+				seconds_timer.innerText = correct_time(seconds_work);
+				textarea.style.color = "red";
+
+				if (pomodoros_count < 1) {
+					let sound = new Audio("assets/end.mp3");
+					sound.play();
+					stop();
+					text_timer.innerText = "Take a long break! Like 15~30 minutes";
+					textarea.style.color = "whitesmoke";
+					textarea.removeAttribute("disabled");
+				} else {
+					let sound = new Audio("assets/work.mp3");
+					sound.play();
+					pomodoros_number.innerText = pomodoros_count;
+				}
+			}
+		}
+	}
+}
+
+function play_timer() {
+	id_interval = setInterval(timer, 1000);
+}
+
+function add_pomodoro() {
+	if (!on_work && !on_break) {
+		pomodoro_qnt++;
+
+		pomodoro.innerText = pomodoro_qnt;
+		pomodoros_number.innerText = pomodoros_count = pomodoro_qnt;
+	}
+}
+
+function subtract_pomodoro() {
+	if (!on_work && !on_break) {
+		pomodoro_qnt--;
+		pomodoro_qnt = (pomodoro_qnt < 1) ? 1 : pomodoro_qnt;
+
+		pomodoro.innerText = pomodoro_qnt;
+		pomodoros_number.innerText = pomodoros_count = pomodoro_qnt;
+	}
+}
+
+function add_work() {
+	if (!on_work && !on_break) {
+		work_qnt++;
+		work_qnt = (work_qnt > 59) ? 59 : work_qnt;
+		minutes_work = work_qnt;
+
+		work_time.innerText = work_qnt;
+		minutes_timer.innerText = correct_time(minutes_work);
+	}	
+}
+
+function subtract_work() {
+	if (!on_work && !on_break) {
+		work_qnt--;
+		work_qnt = (work_qnt < 1) ? 1 : work_qnt;
+		minutes_work = work_qnt;
+
+		work_time.innerText = work_qnt;
+		minutes_timer.innerText = correct_time(minutes_work);
+	}	
+}
+
+function add_break() {
+	if (!on_work && !on_break) {
+		break_qnt++;
+		break_qnt = (break_qnt > 59) ? 59 : break_qnt;
+		minutes_break = break_qnt;
+
+		break_time.innerText = break_qnt;
+	}	
+}
+
+function subtract_break() {
+	if (!on_work && !on_break) {
+		break_qnt--;
+		break_qnt = (break_qnt < 1) ? 1 : break_qnt;
+		minutes_break = break_qnt;
+
+		break_time.innerText = break_qnt;
+	}	
+}
+
+function play() {
+	if (!on_work && !on_break) {
+		on_work = on_play = true;
+		textarea.style.color = "red";
+		textarea.setAttribute("disabled", "");
+		
+		play_timer();
+	} else if (paused) {		
+		on_play = true;
+		paused = false;
+
+		if (on_work) {
+			textarea.style.color = "red";
+		}
+
+		play_timer();
+	}
+}
+
+function pause() {
+	if (on_work || on_break) {
+		on_play = false;
+		paused = true;
+		textarea.style.color = "darkslateblue";
+	}
+}
+
+function stop() {
+	clearInterval(id_interval);
+	on_work = on_break = paused = false;
+	minutes_work = work_qnt;
+	minutes_break = break_qnt;
+	seconds_work = seconds_break = 0;
+	pomodoros_count = pomodoro_qnt;
+	minutes_timer.innerText = correct_time(minutes_work);
+	seconds_timer.innerText = "00";
+	text_timer.innerText = text_work;
+	textarea.style.color = "darkslateblue";
+}
+
+function reset() {
+	clearInterval(id_interval);
+	on_work = on_break = paused = false;
+	work_qnt = minutes_work = default_work_time;
+	break_qnt = minutes_break = default_break_time;
+	pomodoro_qnt = default_pomodoro_qnt;
+	seconds_work = seconds_work = 0;
+
+	work_time.innerText = work_qnt;
+	break_time.innerText = break_qnt;
+	minutes_timer.innerText = minutes_work;
+	seconds_timer.innerText = "00";
+	text_timer.innerText = text_work;
+	pomodoro.innerText = pomodoro_qnt;
+	textarea.style.color = "whitesmoke";
+	textarea.removeAttribute("disabled");
+}
